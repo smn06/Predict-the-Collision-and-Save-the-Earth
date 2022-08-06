80e4fee3c8b70316ee88c6855672a788505870af
Sultan Mahmud Nahian <sultanmahmud621@gmail.com>
2022-08-06 12:05:02 +0600

    Update __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# importing sys
+import sys
+ 
+# adding src/model/ to the system path
+sys.path.insert(0, '/media/smn06/88849B27849B16B2/project/test/Predict the Collision and Save the Earth/src/models')
+ 
+# importing the main
+from __init__ import *
+ 
+main()
